05077bdab72be6be68844d8694fa85f2b47777eb
Brian Howell <brianhowell@pm.me>
2024-02-16 00:56:14 -0500

    implement linked list

--- src/dataStructures/LinkedList/LinkedList.js
+++ src/dataStructures/LinkedList/LinkedList.js
@@ -1,1 +1,78 @@
-// LinkedList
+export class LinkedList {
+  constructor(...values) {
+    this.head = null;
+    this.tail = null;
+    if (values.length) {
+      this.head = this.Node(values[0]);
+      this.tail = this.head;
+      for (let i = 1; i < values.length; i++) {
+        this.tail.next = this.Node(values[i]);
+        this.tail = this.tail.next;
+      }
+    }
+  }
+
+  Node = (value) => ({ value, next: null });
+
+  push = (value) => {
+    const node = this.Node(value);
+    if (!this.head) this.head = node;
+    else this.tail.next = node;
+    this.tail = node;
+  };
+
+  pop = () => {
+    let curr = this.head;
+    while (curr.next !== this.tail) curr = curr.next;
+    curr.next = null;
+    this.tail = curr;
+  };
+
+  shift = (value) => {
+    const node = this.Node(value);
+    node.next = this.head;
+    this.head = node;
+  };
+
+  unshift = () => {
+    this.head = this.head.next;
+  };
+
+  insertAt = (value, index) => {
+    const node = this.Node(value);
+    let curr = this.head;
+    let i = 0;
+    while (i < index - 1) {
+      curr = curr.next;
+      i++;
+    }
+    node.next = curr.next;
+    curr.next = node;
+  };
+
+  deleteAt = (index) => {
+    let curr = this.head;
+    let i = 0;
+    while (i < index - 1) {
+      curr = curr.next;
+      i++;
+    }
+    curr.next = curr.next.next;
+  };
+
+  delete = (value) => {
+    let curr = this.head;
+    while (curr.next.value !== value) curr = curr.next;
+    curr.next = curr.next.next;
+  };
+
+  log = () => {
+    let message = (c) => `${c.value} -> ${c.next ? c.next.value : null}`;
+    let curr = this.head;
+    while (curr) {
+      console.log(message(curr));
+      curr = curr.next;
+    }
+    console.log("-----------------");
+  };
+}
---
+++ src/dataStructures/LinkedList/LinkedList.test.js
@@ -0,0 +1,8 @@
+import "@testing-library/jest-dom";
+import { LinkedList } from "./LinkedList";
+
+describe("LinkedList", () => {
+  it("TEST", () => {
+    expect(1).toBe(1);
+  });
+});
